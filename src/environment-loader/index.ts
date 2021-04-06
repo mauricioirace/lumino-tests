b7e07570712e396b2085a1bd8dc4f9db55098f91
Jonathan Franco <jonaf2103@gmail.com>
2021-04-06 12:43:21 -0300

    Fixing typescript integration

--- src/environment-loader/index.ts
+++ src/environment-loader/index.ts
@@ -1,25 +1,22 @@
-import ContainerManager from "../container-manager";
+import ContainerManager from '../container-manager';
 import {getTokenAddress, validateTokens} from '../token/util';
 import Web3 from 'web3';
-import {LuminoNode, Node, NodeList} from "../types/node";
-import {SetupJson, SetupNode, SetupToken} from "../types/setup";
-import {LuminoTesting} from "../types/lumino-testing";
+import {LuminoNode, NodeList} from '../types/node';
+import {SetupJson, SetupNode, SetupToken} from '../types/setup';
+import {LuminoTestEnvironment} from "../types/lumino-test-environment";
 
-export default class SetupLoader {
+export default class EnvironmentLoader {
 
     private nodes: NodeList = {};
 
     private constructor(private containerManager: ContainerManager) {}
 
-    public static async initialize(setup: SetupJson): Promise<LuminoTesting> {
+    public static async load(setup: SetupJson): Promise<LuminoTestEnvironment> {
         const containerManager = await ContainerManager.create('regtest');
-        const setupLoader: SetupLoader = new SetupLoader(containerManager);
+        const setupLoader = new EnvironmentLoader(containerManager);
         await setupLoader.loadLuminoNodes(setup);
         await setupLoader.openChannels(setup);
-        return {
-            nodes: setupLoader.getNodes,
-            stop: setupLoader.stop
-        };
+        return new LuminoTestEnvironment(setupLoader);
     }
 
     private async loadLuminoNodes(setup: SetupJson): Promise<void> {
