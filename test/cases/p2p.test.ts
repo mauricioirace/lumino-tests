77b08bb420f1299de86dddfb2090c0dc9daca908
mortelli <ortellimarcelo@gmail.com>
2021-04-28 15:45:30 -0300

    add missing closure in p2p test

--- test/cases/p2p.test.ts
+++ test/cases/p2p.test.ts
@@ -1,10 +1,10 @@
 import { tokenAddresses, toWei } from "../../src/util/token";
-import p2p from '../../topologies/p2p.json';
+import p2p from "../../topologies/p2p.json";
 import setupTestEnvironment from "../../src";
 import { LuminoTestEnvironment } from "../../src/types/lumino-test-environment";
 import { LuminoNode } from "../../src/types/node";
 import { Dictionary } from "../../src/util/collection";
-import { Token } from '../../src/constants';
+import { Token } from "../../src/constants";
 import { sleep } from "../utils";
 
 const TIMEOUT = 10 * 60 * 1000;
@@ -22,33 +22,40 @@ describe("Payments", () => {
     await tester.stop();
   }, TIMEOUT);
 
-  it('should make a payment', async () => {
-    const OldInitiatorChannel = await nodes.initiator.client.sdk.getChannel({
-      tokenAddress: tokenAddresses.LUM,
-      partnerAddress: nodes.target.client.address
-    });
-    const OldTargetChannel = await nodes.target.client.sdk.getChannel({
-      tokenAddress: tokenAddresses.LUM,
-      partnerAddress: nodes.initiator.client.address
-    });
-    console.debug(OldInitiatorChannel.balance)
-    console.debug(OldTargetChannel)
-    await nodes.initiator.client.sdk.makePayment({
-      tokenAddress: tokenAddresses.LUM,
-      partnerAddress: nodes.target.client.address,
-      amountOnWei: toWei(1)
-    });
-    await sleep(5000);
-    const initiatorChannel = await nodes.initiator.client.sdk.getChannel({
-      tokenAddress: tokenAddresses.LUM,
-      partnerAddress: nodes.target.client.address
-    });
-    const targetChannel = await nodes.target.client.sdk.getChannel({
-      tokenAddress: tokenAddresses.LUM,
-      partnerAddress: nodes.initiator.client.address
-    });
-    expect(initiatorChannel.balance).toBe(OldInitiatorChannel.balance - toWei(1));
-    expect(targetChannel.balance).toBe(OldTargetChannel.balance + toWei(1));
+  it(
+    "should make a payment",
+    async () => {
+      const OldInitiatorChannel = await nodes.initiator.client.sdk.getChannel({
+        tokenAddress: tokenAddresses.LUM,
+        partnerAddress: nodes.target.client.address,
+      });
+      const OldTargetChannel = await nodes.target.client.sdk.getChannel({
+        tokenAddress: tokenAddresses.LUM,
+        partnerAddress: nodes.initiator.client.address,
+      });
+      console.debug(OldInitiatorChannel.balance);
+      console.debug(OldTargetChannel);
+      await nodes.initiator.client.sdk.makePayment({
+        tokenAddress: tokenAddresses.LUM,
+        partnerAddress: nodes.target.client.address,
+        amountOnWei: toWei(1),
+      });
+      await sleep(5000);
+      const initiatorChannel = await nodes.initiator.client.sdk.getChannel({
+        tokenAddress: tokenAddresses.LUM,
+        partnerAddress: nodes.target.client.address,
+      });
+      const targetChannel = await nodes.target.client.sdk.getChannel({
+        tokenAddress: tokenAddresses.LUM,
+        partnerAddress: nodes.initiator.client.address,
+      });
+      expect(initiatorChannel.balance).toBe(
+        OldInitiatorChannel.balance - toWei(1)
+      );
+      expect(targetChannel.balance).toBe(OldTargetChannel.balance + toWei(1));
+    },
+    TIMEOUT
+  );
 
   it(
     "should make payments in both directions",
