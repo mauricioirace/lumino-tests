a88acd3d614b913b9524e0dad4fc6c22e5e74617
mortelli <ortellimarcelo@gmail.com>
2021-05-03 16:37:57 -0300

    refactor mediated tests

--- test/cases/mediated.test.ts
+++ test/cases/mediated.test.ts
@@ -4,90 +4,82 @@ import setupTestEnvironment from "../../src";
 import { LuminoTestEnvironment } from "../../src/types/lumino-test-environment";
 import { LuminoNode } from "../../src/types/node";
 import { Dictionary } from "../../src/util/collection";
-import { Token } from "../../src/constants";
-import { sleep } from "../utils";
+import { ChannelTestCase, sleep, verifyChannel } from "../utils";
 
-const TIMEOUT = 10 * 60 * 1000;
+const SETUP_TIMEOUT = 5 * 60 * 1000; // 5 minutes
+const TEARDOWN_TIMEOUT = 1 * 60 * 1000; // 1 minute
+const TEST_TIMEOUT = 1 * 60 * 1000; // 1 minute
 
-describe("Mediated Payments", () => {
+describe("mediated payments", () => {
   let nodes: Dictionary<LuminoNode>;
   let tester: LuminoTestEnvironment;
+
+  // starting balance for each node
+  const initiatorDeposit = mediated.channels[0].participant1.deposit;
+  const mediatorDeposit = mediated.channels[0].participant2.deposit;
+  const targetDeposit = mediated.channels[1].participant2.deposit;
+
+  // amount for payment to be made
+  const initiatorPayment = 1;
 
   beforeAll(async () => {
     tester = await setupTestEnvironment(mediated);
     nodes = tester.nodes as Dictionary<LuminoNode>;
-  }, TIMEOUT);
+  }, SETUP_TIMEOUT);
 
   afterAll(async () => {
     await tester.stop();
-  }, TIMEOUT);
+  }, TEARDOWN_TIMEOUT);
 
   it(
-    "should make a payment",
+    "should be able to be made",
     async () => {
-      const oldInitiatorMediatorChannel = await nodes.initiator.client.sdk.getChannel(
-        {
-          tokenAddress: tokenAddresses.LUM,
-          partnerAddress: nodes.mediator.client.address,
-        }
+      const tcInitiator = new ChannelTestCase(
+        tokenAddresses.LUM,
+        nodes.mediator.client.address,
+        toWei(initiatorDeposit),
+        toWei(initiatorDeposit - initiatorPayment),
+        "opened"
       );
-      const oldMediatoInitiatorrChannel = await nodes.mediator.client.sdk.getChannel(
-        {
-          tokenAddress: tokenAddresses.LUM,
-          partnerAddress: nodes.initiator.client.address,
-        }
+
+      const tcMediatorInitiator = new ChannelTestCase(
+        tokenAddresses.LUM,
+        nodes.initiator.client.address,
+        toWei(mediatorDeposit),
+        toWei(mediatorDeposit + initiatorPayment),
+        "opened"
       );
-      const oldMediatorTargetChannel = await nodes.mediator.client.sdk.getChannel(
-        {
-          tokenAddress: tokenAddresses.LUM,
-          partnerAddress: nodes.target.client.address,
-        }
+
+      const tcMediatorTarget = new ChannelTestCase(
+        tokenAddresses.LUM,
+        nodes.target.client.address,
+        toWei(mediatorDeposit),
+        toWei(mediatorDeposit - initiatorPayment),
+        "opened"
       );
-      const oldTargetMediatorChannel = await nodes.target.client.sdk.getChannel(
-        {
-          tokenAddress: tokenAddresses.LUM,
-          partnerAddress: nodes.mediator.client.address,
-        }
+
+      const tcTarget = new ChannelTestCase(
+        tokenAddresses.LUM,
+        nodes.mediator.client.address,
+        toWei(targetDeposit),
+        toWei(targetDeposit + initiatorPayment),
+        "opened"
       );
+
       await nodes.initiator.client.sdk.makePayment({
         tokenAddress: tokenAddresses.LUM,
         partnerAddress: nodes.target.client.address,
-        amountOnWei: toWei(1),
+        amountOnWei: toWei(initiatorPayment),
       });
+
       await sleep(5000);
-      const initiatorMediatorChannel = await nodes.initiator.client.sdk.getChannel(
-        {
-          tokenAddress: tokenAddresses.LUM,
-          partnerAddress: nodes.mediator.client.address,
-        }
-      );
-      const mediatoInitiatorrChannel = await nodes.mediator.client.sdk.getChannel(
-        {
-          tokenAddress: tokenAddresses.LUM,
-          partnerAddress: nodes.initiator.client.address,
-        }
-      );
-      const mediatorTargetChannel = await nodes.mediator.client.sdk.getChannel({
-        tokenAddress: tokenAddresses.LUM,
-        partnerAddress: nodes.target.client.address,
-      });
-      const targetMediatorChannel = await nodes.target.client.sdk.getChannel({
-        tokenAddress: tokenAddresses.LUM,
-        partnerAddress: nodes.mediator.client.address,
-      });
-      expect(initiatorMediatorChannel.balance).toBe(
-        oldInitiatorMediatorChannel.balance - toWei(1)
-      );
-      expect(mediatoInitiatorrChannel.balance).toBe(
-        oldMediatoInitiatorrChannel.balance + toWei(1)
-      );
-      expect(mediatorTargetChannel.balance).toBe(
-        oldMediatorTargetChannel.balance - toWei(1)
-      );
-      expect(targetMediatorChannel.balance).toBe(
-        oldTargetMediatorChannel.balance + toWei(1)
-      );
+
+      // verify both channels from each end
+      await verifyChannel(nodes.initiator.client.sdk, tcInitiator);
+      await verifyChannel(nodes.mediator.client.sdk, tcMediatorInitiator);
+      await verifyChannel(nodes.mediator.client.sdk, tcMediatorTarget);
+      await verifyChannel(nodes.target.client.sdk, tcTarget);
     },
-    TIMEOUT
+    TEST_TIMEOUT
   );
 });
